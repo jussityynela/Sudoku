228366cf57038279392e6763008265b827d4d67f
somename <someemail>
2015-10-02 12:25:25 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuException.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuException.java
@@ -2,4 +2,9 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuException extends RuntimeException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1092847391696957077L;
+
 }
