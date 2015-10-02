8de430771d41be2484dbeb13e86e7e5a8d8feca6
somename <someemail>
2015-10-02 12:43:17 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 
 	private static final String correctSudokuString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-	private static final String SudokuStringWithNegativeNumber = "417-369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private static final String SudokuStringWithNegativeNumber = "417-36982563215894795872431682543716979158643234691275828964357157329168416487529";
 	private static final String incorrectSudokuString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	@Test(expected=SudokuStringTooLongException.class)
 	public void testStringTooLongException() {
