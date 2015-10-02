c748dd8aafec9a3b7882d6a965a5898cf6066a02
somename <someemail>
2015-10-02 12:25:50 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuStringEmptyException.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuStringEmptyException.java
@@ -2,4 +2,9 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuStringEmptyException extends SudokuException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8335003949212887564L;
+
 }
