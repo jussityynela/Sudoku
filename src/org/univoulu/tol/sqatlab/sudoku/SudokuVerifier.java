2c63289875e54246ea335ab8e312b408ff9cc368
somename <someemail>
2015-10-02 12:26:43 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -4,12 +4,13 @@ public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
 		// returns 1 if the candidate solution is correct
-		if( candidateSolution.length() > 81 )
-			throw new SudokuStringTooLongException();
-		if( candidateSolution.length() < 81 && !candidateSolution.isEmpty() )
-			throw new SudokuStringTooShortException();
 		if( candidateSolution.isEmpty() )
 			throw new SudokuStringEmptyException();
+		else if( candidateSolution.length() > 81 )
+			throw new SudokuStringTooLongException();
+		else if( candidateSolution.length() < 81 )
+			throw new SudokuStringTooShortException();
+		
 		return 0;
 	}
 }
