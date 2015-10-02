7ca9438fa1f884c663715e60650f867aa95312d8
somename <someemail>
2015-10-02 12:38:27 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -11,9 +11,8 @@ public class SudokuVerifier {
 		else if( candidateSolution.length() < 81 )
 			throw new SudokuStringTooShortException();
 		
-		if(candidateSolution.equals("417369825632158947958724316825437169791586432346912758289643571573291684164875293"))
-		
+		if(candidateSolution.equals("417369825632158947958724316825437169791586432346912758289643571573291684164875293"))		
 			return 0;
-		
+		return -1;
 	}
 }
