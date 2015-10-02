ccacf7d10bdd7bea96f8b910f56cabebde2aba8c
somename <someemail>
2015-10-02 12:17:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuStringTooLongException.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuStringTooLongException.java
@@ -2,4 +2,9 @@ package org.univoulu.tol.sqatlab.sudoku;
 
 public class SudokuStringTooLongException extends SudokuException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 }
