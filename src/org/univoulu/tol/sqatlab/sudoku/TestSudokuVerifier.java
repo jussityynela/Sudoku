ad149f019b4e77eafb96749e3bbfe73ab5a28b85
somename <someemail>
2015-10-02 12:41:31 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 
 	private static final String correctSudokuString = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
-	private static final String correctSudokuString = "417-369825632158947958724316825437169791586432346912758289643571573291684164875293";
-	private static final String correctSudokuString = "417-369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private static final String SudokuStringWithNegativeNumber = "417-369825632158947958724316825437169791586432346912758289643571573291684164875293";
+	private static final String incorrectSudokuString = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
 	@Test(expected=SudokuStringTooLongException.class)
 	public void testStringTooLongException() {
 		SudokuVerifier sudokuVerifier = new SudokuVerifier();
