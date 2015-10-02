e34619304e8dd53bcf43c89d6f365c1fe0db0b2b
somename <someemail>
2015-10-02 12:50:19 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -13,7 +13,7 @@ public class SudokuVerifier {
 		//First split the string to be in rows steps
 		String[] sudokuCanditeRows= new String[9];
 		for(int i =0; i<9;++i)
-			sudokuCanditeRows[i] = candidateSolution.substring(0, 9);
+			sudokuCanditeRows[i] = candidateSolution.substring(i, 9);
 		if(candidateSolution.equals("417369825632158947958724316825437169791586432346912758289643571573291684164875293"))		
 			return 0;
 		return -1;
