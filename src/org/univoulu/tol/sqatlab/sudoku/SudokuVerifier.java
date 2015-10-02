487caf5fd59054254d3176204a832a5a5c88d319
somename <someemail>
2015-10-02 13:02:46 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -21,7 +21,7 @@ public class SudokuVerifier {
 		 
 		if(candidateSolution.equals("417369825632158947958724316825437169791586432346912758289643571573291684164875293"))		
 			return 0;
-		return -1;
+		return -5;
 	}
 	
 	public static List<String> splitEqually(String text, int size) {
